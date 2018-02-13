3c3f5a98493a79424e0e6d132b52fa6e05f6bcf7
Olubodun Agbalaya <s.stackng@gmail.com>
2018-02-13 08:02:31 +0100

    json output correction

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+"use strict";
 const express = require('express');
 const redis = require('redis');
 
@@ -25,7 +26,7 @@ const app = express();
 // index page, shows all cached data
 app.get('/', (req, res)=> {
 
-client.exists('demo-app-db', function (err, reply) {
+    client.exists('demo-app-db', function (err, reply) {
 
     if (reply === 1) {
 
@@ -49,7 +50,7 @@ client.exists('demo-app-db', function (err, reply) {
             {}, (err, reply)=> {
 
             console.log(reply);
-        })
+    })
         ;
 
         res.send('No data inmemory, <br/> Defaut storage structure allocated');
@@ -61,146 +62,153 @@ client.exists('demo-app-db', function (err, reply) {
 })
 ;
 
-// create cached data
-app.post('/api/add/:key/:value', (req, res) => {
-				
-				let data_key = req.params.key;
-				const new_data ={};
-				new_data[data_key]  = req.params.value;
-			
-			client.hmset('demo-app-db', new_data,//allocate redis storage
-          
-				(err, reply)=> {
-	
-				if(reply){
-							res.send("<br/>Added key: "+data_key +" with value: "+req.params.value ).end();
+// create  data
+app.get('/api/add/:key/:value', (req, res) => {
 
-				}else{
-					res.send("<br/>Error adding new key/value pair").end();		
-				}
+    let data_key = req.params.key;
+const new_data ={};
+new_data[data_key]  = req.params.value;
 
-			})
-	
+client.hmset('demo-app-db', new_data,//allocate redis storage
+
+    (err, reply)=> {
+
+    if(reply){
+        res.send({response:201, data: "OK"}).end();
+
+    }else{
+        res.send({response:500}).end();
+}
+
+})
+
 })
 ;
 
 // read single key/value
 app.get('/api/read/:key', (req, res) => {
-		
-		client.hgetall("demo-app-db", (err, result)=> {
 
-            if(result) {
-				
-				if(result[req.params.key]){
-					
-                res.send("Cached value of key: "+req.params.key + " is --" +result[req.params.key]).end();
-				console.log("Single data retrieved "+req.params.key+"=>"+result[req.params.key]);
-				}
-				else{
-						res.send("Key: *"+req.params.key+"* Not found").end();
-						console.log("Key: *"+req.params.key+"* Not found");
-				}
-                
-	
-            }
-            else{
-					res.send("An error occured :: " + err).end();
-				}
-				
-	    })
+    client.hgetall("demo-app-db", (err, result)=> {
 
-	
-	
+    if(result) {
+
+        if(result[req.params.key]){
+
+
+            res.send({response:200, data: result[req.params.key]}).end();
+
+
+            console.log("Single data retrieved "+req.params.key+"=>"+result[req.params.key]);
+        }
+        else{
+            res.send({response:404, data: "Key: *"+req.params.key+"* Not found"}).end();
+            console.log("Key: *"+req.params.key+"* Not found");
+        }
+
+
+    }
+    else{
+        res.send({response:500}).end();
+	}
+
+	})
+
+
+
 })
 ;
 
 // update cached data
-app.post('/api/update/:key/:value', (req, res) => {
-		
-		client.hgetall("demo-app-db", (err, result)=> {
+app.get('/api/update/:key/:value', (req, res) => {
 
-            if(result) {
-				
-				if(result[req.params.key]){
-				
-					let data_key = req.params.key;
-					const new_data ={};
-					new_data[data_key]  = req.params.value;
-			
-					client.hmset('demo-app-db', new_data,//allocate redis storage
-          
-						(err, reply)=> {
-	
-						if(reply){
-							res.send("<br/>Updated key: "+data_key +" with value: "+req.params.value ).end();
-							console.log("Data updated");
+    client.hgetall("demo-app-db", (err, result)=> {
 
-						}else{
-							res.send("<br/>Error updating new key/value pair").end();		
-							console.log("Update failed");		
-						}
+    if(result) {
 
-					});
-                
-                
-	
-            }else{
-							res.send("Key: *"+req.params.key+"* Not found, nothing to update").end();
-						console.log("Update Key: *"+req.params.key+"* Not found");
-			
-			}
-		}
-        else{
-					res.send("An error occured :: " + err).end();
-			}
-				
-	    })
+        if(result[req.params.key]){
 
-	
-	
+            let data_key = req.params.key;
+            const new_data ={};
+            new_data[data_key]  = req.params.value;
+
+            client.hmset('demo-app-db', new_data,//allocate redis storage
+
+                (err, reply)=> {
+
+                if(reply){
+
+                    res.send({response:200, data:"ENTRY_UPDATED"}).end();
+                    console.log("Data updated");
+
+                }else{
+                    res.send({response:500}).end();
+            console.log("Update failed");
+        }
+
+        });
+
+        }else{
+
+            res.send({response:404, data: "KEY_NOT_FOUND"}).end();
+            console.log("Update Key: *"+req.params.key+"* Not found");
+
+        }
+
+
+    }else{
+        res.send({response:500}).end();
+
+}
+
+
+})
+
+
+
 })
 ;// delete cached data
-app.post('/api/delete/:key', (req, res) => {
-		
-		client.hgetall("demo-app-db", (err, result)=> {
+app.get('/api/delete/:key', (req, res) => {
 
-            if(result) {
-				
-				if(result[req.params.key]){
-				
-					let data_key = req.params.key;
-			
-					client.hdel('demo-app-db', data_key,//allocate redis storage
-          
-						(err, reply)=> {
-	
-						if(reply){
-							res.send("<br/>Deleted entry with key: "+data_key ).end();
-							console.log("Data updated");
+    client.hgetall("demo-app-db", (err, result)=> {
 
-						}else{
-							res.send("<br/>Error deleting  key/value pair").end();		
-							console.log("Update failed");		
-						}
+    if(result) {
 
-					});
-                
-                
-	
-            }else{
-							res.send("Key: *"+req.params.key+"* Not found, nothing to delete").end();
-						console.log("Delete Key: *"+req.params.key+"* Not found");
-			
-			}
-		}
-        else{
-					res.send("An error occured :: " + err).end();
-			}
-				
-	    })
+        if(result[req.params.key]){
 
-	
-	
+            let data_key = req.params.key;
+
+            client.hdel('demo-app-db', data_key,//allocate redis storage
+
+                (err, reply)=> {
+
+                if(reply){
+                    res.send({response:200, data: "ENTRY_DELETED"}).end();
+                    console.log("Data updated");
+
+                }else{
+                    res.send({response:500}).end();
+
+            console.log("Update failed");
+        }
+
+        });
+
+
+
+        }else{
+            res.send({response:404, data: "KEY_NOT_FOUND"}).end();
+            console.log("Delete Key: *"+req.params.key+"* Not found");
+
+        }
+    }
+    else{
+        res.send({response:500}).end();
+}
+
+})
+
+
+
 })
 ;
 
